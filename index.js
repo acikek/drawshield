003f457837a58568dab4faa6b926e65671819969
acikek <spiralixp@gmail.com>
2020-10-14 16:56:33 -0700

    Refactored to not use idiotic Client class lmao

--- index.js
+++ index.js
@@ -2,67 +2,54 @@ const fs = require("fs");
 const svgToImg = require("svg-to-img");
 const axios = require("axios");
 
-class Client {
-  /**
-   * Creates a new drawshield Client instance.
-   * 
-   * @param {Object} options
-   */
+/**
+ * Generates a blazoned shield and saves it to a PNG file.
+ * 
+ * @param {String} blazon 
+ * @param {Object} [options]
+ * @param {String} [options.dir="./blazons"]
+ * @param {String} [options.filename]
+ * @param {Boolean} [options.save]
+ * @returns {Buffer} Image buffer
+ */
 
-  constructor(options) {
-    this.directory = `${options.dir}` || "./",
-    this.save = options.save || true
+async function drawShield(blazon, { dir = "./blazons", filename, save }) {
+  let svg = await axios.get(`https://drawshield.net/include/drawshield.php?size=500&blazon=${encodeURIComponent(blazon)}`);
+  svg = svg.data.replace(`id="test1"`, `id="test1" visibility="hidden"`).replace(`id="release-id"`, `id="release-id" visibility="hidden"`);
+  let image = await svgToImg.from(svg).toPng();
+
+  if (save != false) {
+    fs.writeFileSync(`${dir}/${filename || blazon.replace(/ /g, "-").replace(/,/g, "")}.png`, image);
   }
 
-  /**
-   * Generates a blazoned shield and saves it to a PNG file.
-   * 
-   * @param {String} blazon 
-   * @param {String} dir
-   * @param {String} filename
-   * @returns {Buffer} Image buffer
-   */
-
-  async drawShield(blazon, dir, filename) {
-    let directory = dir || this.directory;
-
-    let svg = await axios.get(`https://drawshield.net/include/drawshield.php?size=500&blazon=${encodeURIComponent(blazon)}`);
-    svg = svg.data.replace(`id="test1"`, `id="test1" visibility="hidden"`).replace(`id="release-id"`, `id="release-id" visibility="hidden"`);
-    let image = await svgToImg.from(svg).toPng();
-
-    if (this.save) {
-      let fileName = filename || blazon.replace(/ /g, "-").replace(/,/g, "");
-      fs.writeFileSync(`${directory}/${fileName}.png`, image);
-    }
-
-    return image;
-  }
-
-  /**
-   * Returns the data from a DrawShield.net term.
-   * 
-   * @param {String} term 
-   * @param {String} source 
-   * @param {String} match 
-   * @returns {Object} Data object
-   */
-
-  async fetchTerm(term, source, match) {
-    let src = source ? `&source=${source.toLowerCase()}` :  "";
-    let m = match ? `&match=${match.toLowerCase()}` : "";
-
-    let result = await axios.get(`https://drawshield.net/api/define/${term}${src}${m}`);
-    if (result.data.error) throw new Error(result.data.error);
-
-    return {
-      content: result.data.content,
-      URL: result.data.URL,
-      trueURL: `https://drawshield.net/api/define/${term}${src}${m}`,
-      term: term.toLowerCase(),
-      source: source ? source.toLowerCase() : null,
-      match: source ? match.toLowerCase() : null
-    };
-  }
+  return image;
 }
 
-module.exports = { Client }; 
+/**
+ * Returns the data from a DrawShield.net term.
+ * 
+ * @param {String} term 
+ * @param {Object} [options]
+ * @param {String} [options.source]
+ * @param {String} [options.match]
+ * @returns {Object} Data object
+ */
+
+async function fetchTerm(term, { source, match }) {
+  let src = source ? `&source=${source.toLowerCase()}` :  "";
+  let m = match ? `&match=${match.toLowerCase()}` : "";
+
+  let result = await axios.get(`https://drawshield.net/api/define/${term}${src}${m}`);
+  if (result.data.error) throw new Error(result.data.error);
+
+  return {
+    content: result.data.content,
+    URL: result.data.URL,
+    trueURL: `https://drawshield.net/api/define/${term}${src}${m}`,
+    term: term.toLowerCase(),
+    source: source ? source.toLowerCase() : null,
+    match: source ? match.toLowerCase() : null
+  };
+}
+
+module.exports = { drawShield, fetchTerm }; 
--- readme.md
+++ readme.md
@@ -9,29 +9,23 @@ npm install --save drawshield
 
 ## Example
 ```js
-const DS = require("drawshield");
-
-// Create a new Client instance
-const client = new DS.Client({
-  dir: "./shields",
-  save: true
-});
+const { drawShield, fetchTerm } = require("drawshield");
 
 // Create a few blazons
-client.drawShield("azure, a fess argent", "./new-shields", "my-shield");
-client.drawShield("gules, a five pointed star gold");
-client.drawShield("quarterly I. and IV. gules a castle or II. and III. argent a lion rampant purpure");
+drawShield("azure, a fess argent", { dir: "./new-shields", filename: "my-shield" });
+drawShield("gules, a five pointed star gold");
+drawShield("quarterly I. and IV. gules a castle or II. and III. argent a lion rampant purpure");
 
 // Asynchronously return the image data from a blazon
 (async function() {
-  let result = await client.drawShield("per cross gules and argent");
+  let result = await drawShield("per cross gules and argent", { save: false });
   console.log(result);
   // SVG Info
 })();
 
 // Asynchronously return the data from a term lookup
 (async function() {
-  let result = await client.fetchTerm("pall", "parker", "exact");
+  let result = await fetchTerm("pall", { source: "parker", match: "exact" });
   console.log(result);
   /* { 
    *    content: "Pall, (fr. pairle, which is also occasionally used by English heralds):",
@@ -48,36 +42,30 @@ client.drawShield("quarterly I. and IV. gules a castle or II. and III. argent a 
 
 ## Documentation
 
-### `Client(options)`
-The DrawShield Client instance.
-
-#### Params
-
-- **Object** `options`: An object containing the following fields:
- - `dir` (String): The directory in which images are saved
- - `save` (Boolean): Determines whether or not images are saved
-
-### `Client.drawShield(blazon, dir, filename)`
+### `drawShield(blazon, options)`
 Creates a blazon using DrawShield.net and saves it to a PNG file.
 
 #### Params
 
 - **String** `blazon`: The input using DrawShield.net's blazon format
-- **String** `dir`: Specifies the directory in which the image will be saved, if needed
-- **String** `filename`: Specifies the filename - if left blank, uses the blazon input
+- **Object** `options`: An object containing the following fields:
+  - `dir` (String): Specifies the directory in which the image will be saved, if needed
+  - `filename` (String): Specifies the filename - if left blank, uses the blazon input
+  - `save` (Boolean): If the file shouldn't be saved
 
 #### Return
 
 - **Object**: The SVG image data in XML format
 
-### `Client.fetchTerm(term, source, match)`
+### `fetchTerm(term, options)`
 Returns the data from a heraldic term lookup.
 
 #### Params
 
 - **String** `term`: Heraldic term input
-- **String** `source`: Specifies the heraldic dictionary to lookup from (parker, elvin)
-- **String** `match`: Specifies the term matching type (exact, fuzzy)
+- **Object** `options`: An object containing the following fields:
+  - `source` (String): Specifies the heraldic dictionary to lookup from (parker, elvin)
+  - `match` (String): Specifies the term matching type (exact, fuzzy)
 
 #### Return
 
